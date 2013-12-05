526d022ca5730065be0edc481d386f96385b6cd8
ka <ka.kaosf@gmail.com>
2013-12-06 02:44:26 +0900

    fix to avoid an error
    error log:
    E/AndroidRuntime(10965):        at net.kaosfield.app.MainActivity.onCreate(MainActivity.java:12)
    E/AndroidRuntime(10965): Caused by: java.lang.ClassNotFoundException: android.view.Fragment in loader dalvik.system.PathClassLoader[/data/app/net.kaosfield.app-1.apk]
    
    import android.support.v4.app.FragmentActivity
    
    DO NOT import com.google.android.gms.maps.SupportMapFragment
    
    extend FragmentActivity instead of Activity
    
    modify "android:name" to "class" in layout/fragment_*.xml
    
    ref. http://stackoverflow.com/questions/18557706/java-lang-classnotfoundexception-in-dalvik-system-pathclassloader-findclass

--- src/net/kaosfield/app/MainActivity.java
+++ src/net/kaosfield/app/MainActivity.java
@@ -3,8 +3,9 @@ package net.kaosfield.app;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.support.v4.app.FragmentActivity;
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
