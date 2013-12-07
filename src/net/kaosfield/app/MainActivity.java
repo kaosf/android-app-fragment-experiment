4f1662d1e17970f49fc495f0cd52dcf7dfe63b2d
ka <ka.kaosf@gmail.com>
2013-12-07 17:37:44 +0900

    add 3 tabs
    ref. http://developer.android.com/training/implementing-navigation/lateral.html

--- src/net/kaosfield/app/MainActivity.java
+++ src/net/kaosfield/app/MainActivity.java
@@ -1,6 +1,8 @@
 package net.kaosfield.app;
 
 import android.app.ActionBar;
+import android.app.FragmentTransaction;
+import android.util.Log;
 import android.os.Bundle;
 import android.view.Menu;
 import android.support.v4.app.FragmentActivity;
@@ -14,6 +16,32 @@ public class MainActivity extends FragmentActivity {
 
     final ActionBar actionBar = getActionBar();
     actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+    ActionBar.TabListener tabListener = new ActionBar.TabListener() {
+      public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
+        // show the given tab
+        Log.d("kaosfield", "onTabSelected");
+      }
+
+      public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
+        // hide the given tab
+        Log.d("kaosfield", "onTabUnselected");
+      }
+
+      public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
+        // probably ignore this event
+        Log.d("kaosfield", "onTabReselected");
+      }
+    };
+
+    // Add 3 tabs, specifying the tab's text and TabListener
+    for (int i = 0; i < 3; i++) {
+      actionBar.addTab(
+        actionBar.newTab()
+          .setText("Tab " + (i + 1))
+          .setTabListener(tabListener)
+      );
+    }
   }
 
   @Override
